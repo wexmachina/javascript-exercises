781f07e36099d9980dbd3b2fd2d10f2a7bfe6721
Wesley Williams <wesw88@gmail.com>
2022-12-07 22:27:26 -0500

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    if ((string.split("").reverse().join("").replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .replace(/\s{2,}/g, " ").toLowerCase().replaceAll(' ', '')) == string.replaceAll(' ','').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .replace(/\s{2,}/g, " ").toLowerCase())
+     return true;
+    else return false;
 };
 
 // Do not edit below this line
