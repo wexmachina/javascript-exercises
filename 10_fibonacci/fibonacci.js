939e9bf1227af18fb03f91209f9e8c2fd6b24559
Wesley Williams <wesw88@gmail.com>
2022-12-24 17:05:17 -0500

    commit

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let num = parseInt(input);
+    if (num<1) {
+        return "OOPS";
+    }
+    let a=0, b=1, c=num;
 
+    for (let i=2; i<=num; i++) {
+        c=a+b;
+        a=b;
+        b=c;
+    }
+    return c
 };
 
 // Do not edit below this line
